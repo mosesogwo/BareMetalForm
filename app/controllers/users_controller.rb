fb59df42fc1583d56692d185bf3b98bbb2765c93
uche-inyama <depoulet@yahoo.com>
2019-08-21 15:49:00 +0100

    stickler edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -12,9 +13,11 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
   def edit
     @user = User.find(params[:id])
   end
+
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
@@ -25,6 +28,7 @@ class UsersController < ApplicationController
   end
 
   private
+  
     def user_params
       params.require(:user).permit(:username, :email, :password)
     end
