020982b1b568a35549d71c6a9f061677f673927c
uche-inyama <depoulet@yahoo.com>
2019-08-21 15:50:05 +0100

    stickler edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
   private
   
-    def user_params
-      params.require(:user).permit(:username, :email, :password)
-    end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
